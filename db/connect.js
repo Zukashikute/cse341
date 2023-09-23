03947e97beb9695f0deac39c8b216524c3127d00
Jared Joshua Balajadia <46180962+Zukashikute@users.noreply.github.com>
2023-09-23 13:19:46 +0800

    Fix

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
 const { MongoClient } = require('mongodb');
 
-const mongoURL = `${process.env.MONGO_URL}`;
+const mongoURL = process.env.MONGO_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(mongoURL);
